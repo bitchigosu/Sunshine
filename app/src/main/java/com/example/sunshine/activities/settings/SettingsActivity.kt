e604d885e26aa083c38d07a972efb404311866fd
bitchigosu <kostya96.08@mail.ru>
2019-04-26 23:14:13 +0300

    Added poweredBy message with linking

--- app/src/main/java/com/example/sunshine/activities/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/sunshine/activities/settings/SettingsActivity.kt
@@ -1,6 +1,14 @@
 package com.example.sunshine.activities.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.util.Linkify
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sunshine.R
 import kotlinx.android.synthetic.main.activity_settings.*
@@ -13,5 +21,27 @@ class SettingsActivity : AppCompatActivity() {
         back_image.setOnClickListener {
             finish()
         }
+        powered_by_darksky.apply {
+            movementMethod = LinkMovementMethod.getInstance()
+            val darkskyUrl = "https://darksky.net/poweredby/"
+            val message = SpannableString(getString(R.string.powered_by_darksky)).apply {
+                setLinkSpan("DarkSky",darkskyUrl)
+            }
+            text = message
+        }
+    }
+
+    private fun SpannableString.setLinkSpan(text: String, url: String) {
+        val textIndex = this.indexOf(text)
+        setSpan(
+            object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    Intent(Intent.ACTION_VIEW).apply { data = Uri.parse(url) }.also { startActivity(it) }
+                }
+            },
+            textIndex,
+            textIndex + text.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
     }
 }
