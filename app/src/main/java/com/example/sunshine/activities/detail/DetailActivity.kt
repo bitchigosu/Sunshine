73bc05829e0b13486359831378d2042dce871357
bitchigosu <kostya96.08@mail.ru>
2019-05-02 23:41:11 +0300

    Added data binding to detail activity

--- app/src/main/java/com/example/sunshine/activities/detail/DetailActivity.kt
+++ app/src/main/java/com/example/sunshine/activities/detail/DetailActivity.kt
@@ -6,9 +6,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.sunshine.R
 import com.example.sunshine.ViewModelFactory
 import com.example.sunshine.activities.settings.SettingsActivity
+import com.example.sunshine.databinding.ActivityDetailBinding
 import com.example.sunshine.utils.JsonUtil
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.extra_weather_detail.*
@@ -22,8 +24,7 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-
+        val binding: ActivityDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
         position = intent.getIntExtra("WeatherPos", 0)
 
         back_image.setOnClickListener {
@@ -43,16 +44,21 @@ class DetailActivity : AppCompatActivity() {
                             JsonUtil.getIcon(data.getIconId())
                         )
                     )
-                    day_text.text = data.getDate()
-                    weather_description_text.text = data.getWeatherDesc()
-                    max_temp_text.text = data.getMaxTemp().toString() + getString(R.string.temp_symbol)
-                    min_temp_text.text = data.getMinTemp().toString() + getString(R.string.temp_symbol)
-                    pressure_value.text = data.getPressure().toInt().toString() + " " + getString(R.string.hpa)
-                    wind_value.text = data.getWindSpeed().toString() + " " + getString(R.string.kph)
-                    humidity_value.text = data.getHumidity().toString() + getString(R.string.percentage_symbol)
-                    sunrise_value.text = data.getSunrise()
-                    sunset_value.text = data.getSunset()
-                    uvIndex_value.text = data.getUvIndex().toString()
+                    with(binding.primaryInfo) {
+                        dayText.text = data.getDate()
+                        weatherDescriptionText.text = data.getWeatherDesc()
+                        maxTempText.text = data.getMaxTemp().toString() + getString(R.string.temp_symbol)
+                        minTempText.text = data.getMinTemp().toString() + getString(R.string.temp_symbol)
+                    }
+
+                    with(binding.extraDetails) {
+                        sunriseValue.text = data.getSunrise()
+                        sunsetValue.text = data.getSunset()
+                        humidityValue.text = data.getHumidity().toString() + getString(R.string.percentage_symbol)
+                        pressureValue.text = data.getPressure().toString()
+                        windValue.text = data.getWindSpeed().toString() + getString(R.string.kph)
+                        uvIndexValue.text = data.getUvIndex().toString()
+                    }
                 }
             }
         })
