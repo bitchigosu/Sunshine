d430245a79155338814c4aee8424fd98e0ffaff8
kkgosu <kostya96.08@mail.ru>
2021-05-13 18:33:51 +0300

    gradle full update, details activity fix

--- app/src/main/java/com/example/sunshine/activities/detail/DetailActivity.kt
+++ app/src/main/java/com/example/sunshine/activities/detail/DetailActivity.kt
@@ -4,66 +4,83 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.example.sunshine.R
 import com.example.sunshine.ViewModelFactory
 import com.example.sunshine.activities.settings.SettingsActivity
 import com.example.sunshine.databinding.ActivityDetailBinding
 import com.example.sunshine.utils.JsonUtil
-import kotlinx.android.synthetic.main.primary_weather_info.*
-
 
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDetailBinding
+    private lateinit var viewModel: DetailViewModel
+
     private var position: Int = 0
-    private lateinit var mViewModel: DetailViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val binding: ActivityDetailBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_detail)
-        mViewModel = ViewModelProviders.of(this, ViewModelFactory()).get(DetailViewModel::class.java)
-        binding.viewModel = mViewModel
-
-        mViewModel.goToBackScreen.observe(this, Observer {
-            finish()
-        })
-
-        mViewModel.goToSettingsScreen.observe(this, Observer {
-            navigateToSettings()
-        })
-
+        setupView()
+        subscribeToObservers()
         position = intent.getIntExtra("WeatherPos", 0)
-        mViewModel.getWeatherById(position).observe(this, Observer {
-            it?.let { data ->
-                with(this@DetailActivity) {
-                    forecast_icon.setImageDrawable(
-                        ContextCompat.getDrawable(
-                            this@DetailActivity,
-                            JsonUtil.getIcon(data.getIconId())
-                        )
-                    )
+    }
+
+    private fun setupView() {
+        with(binding) {
+            backImage.setOnClickListener {
+                viewModel.onBackClicked()
+            }
+
+            settingsImage.setOnClickListener {
+                viewModel.onSettingsClicked()
+            }
+        }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel = ViewModelProvider(this, ViewModelFactory()).get(DetailViewModel::class.java)
+        with(viewModel) {
+            goToBackScreen.observe(this@DetailActivity) {
+                finish()
+            }
+            goToSettingsScreen.observe(this@DetailActivity) {
+                navigateToSettings()
+            }
+            getWeatherById(position).observe(this@DetailActivity) {
+                it?.let { data ->
+
                     with(binding.primaryInfo) {
+                        forecastIcon.setImageDrawable(
+                            ContextCompat.getDrawable(
+                                this@DetailActivity,
+                                JsonUtil.getIcon(data.getIconId())
+                            )
+                        )
                         dayText.text = data.getDate()
                         weatherDescriptionText.text = data.getWeatherDesc()
-                        maxTempText.text = data.getMaxTemp().toString() + getString(R.string.temp_symbol)
-                        minTempText.text = data.getMinTemp().toString() + getString(R.string.temp_symbol)
+                        maxTempText.text =
+                            "${data.getMaxTemp()}${getString(R.string.temp_symbol)}"
+                        minTempText.text =
+                            "${data.getMinTemp()}${getString(R.string.temp_symbol)}"
                     }
 
                     with(binding.extraDetails) {
                         sunriseValue.text = data.getSunrise()
                         sunsetValue.text = data.getSunset()
-                        humidityValue.text = data.getHumidity().toString() + getString(R.string.percentage_symbol)
-                        pressureValue.text = data.getPressure().toInt().toString() + getString(R.string.hpa)
-                        windValue.text = data.getWindSpeed().toString() + getString(R.string.kph)
-                        uvIndexValue.text = data.getUvIndex().toString()
+                        humidityValue.text =
+                            "${data.getHumidity()}${getString(R.string.percentage_symbol)}"
+                        pressureValue.text =
+                            "${data.getPressure().toInt()}${getString(R.string.hpa)}"
+                        windValue.text = "${data.getWindSpeed()}${getString(R.string.kph)}"
+                        uvIndexValue.text = "${data.getUvIndex()}"
                     }
                 }
+
             }
-        })
+        }
     }
 
     private fun navigateToSettings() {
